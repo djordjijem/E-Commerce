900417b6201f12b0c0ba7ce221426142781603e8
djordjijem <djordjijem@gmail.com>
2019-10-08 23:21:47 +0200

    Ch: conntect snipcart api

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,26 @@ import ProductList from '../components/ProductList';
 import { IProduct } from '../components/Product';
 import Footer from '../components/Footer';
 import Head from 'next/head';
+import { API_KEY } from '../api';
 interface IIndexProps {
   products: IProduct[];
 }
 const Index = (props: IIndexProps) => {
   return (
     <div className='app'>
+      <Head>
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js'></script>
+        <script
+          src='https://cdn.snipcart.com/scripts/2.0/snipcart.js'
+          data-api-key={`${API_KEY}`}
+          id='snipcart'
+        ></script>
+        <link
+          href='https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css'
+          rel='stylesheet'
+          type='text/css'
+        />
+      </Head>
       <Header />
       <main className='main'>
         <ProductList products={props.products} />
